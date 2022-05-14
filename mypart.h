6c67420d2b024dd76cd1662f96b6c53779e23c9b
AZH_PinRui <bill880710@gmail.com>
2022-05-14 20:45:56 +0800

    Pin Rui's part is basically done
    Every function needed to manipulate the data in the linked list is done, please check.
    Contact me if there's any error or problem.

--- mypart.h
+++ mypart.h
@@ -3,6 +3,7 @@
 #include "date.h"
 #include "spend.h"
 #define DAY_OF_MONTH 31
+#define EXTRACT_UPPER_LIMIT 500
 
 /*LINKED LIST PART*/
 /*
@@ -36,6 +37,7 @@ void deleteData(struct LLNode *list);
 void printLL(struct LLNode *head);
 
 /*OCCURENCE LIST PART*/
+
 typedef struct Occurence{
     int initPos; //index of first position of that Date.
     int Count;   //How many repeated data there are.
@@ -47,8 +49,41 @@ int isSameDate(Date date1, Date date2);
 Occurence *findOccurence(struct LLNode *sortedList);
 //Testing function to print occurence list.
 void printOccurence(Occurence *occurenceList, int size);
+/*
+Function to find actual ending index of occurenceList.
+To avoid possible index overflow in function getKeyData.
+*/
+int findOccurTail(Occurence *occurenceList);
 
 
 
-/*Data extraction part*/
+/*DATA EXTRACTION PART*/
 
+/*
+keyDataList has the following members:
+extractionType : This indicates what type of data we need. (certain day interval? certain category? or both?)
+keyCategory : the category we're looking for, -1 if the category is not specified.
+dayBegin,dayEnd : day interval, shoule be easy.
+dataList : it's an array of struct Spend, this is the data we extract from the linked list.
+*/
+typedef struct{
+    short int extractionType;
+    short int keyCategory; //we may have -1, so we're not using enum.
+    short int dayBegin, dayEnd;
+    short int listLength;
+    Spend *dataList; 
+}keyDataList;
+
+/*
+This is a general data extraction function, you can choose to :
+1. Enter only keyCategory, leaving both day 0 to get data with certain category.
+2. Enter only dayBegin and dayEnd, leaving keyCategory -1 to get data of certain interval
+3. Enter all of those to get data within certain interval and of certain category.
+NOTE: If you want the data from 1 specific day, type same number in dayBegin and dayEnd !
+NOTE: REMEMBER IF YOU DON'T NEED dayBegin and dayEnd OPTION, BE SURE TO LEAVE them AS 0 !!
+AND IF YOU DON'T NEED keyCategory, LEAVE keyCategory AS -1 !
+*/
+keyDataList *getKeyData(struct LLNode *sortedList, Occurence *occurenceList, Category keyCategory, const short dayBegin, const short dayEnd);
+
+//testing function to print designated keyList.
+void printKeyList(keyDataList *keyList);
