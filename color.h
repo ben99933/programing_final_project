5ffc1f285f9cf9fa01e15d06896706d88f3ebbfe
ben99933 <yangben999@gmail.com>
2022-05-30 21:01:36 +0800

    text color

--- color.h
+++ color.h
@@ -1,6 +1,12 @@
 #ifndef COLOR_H
     #define COLOR_H
 
+    /**
+     * 如果你要直接調用這些Color在printf裡面的話
+     * 利用 "string" <color> "string2"的特性
+     * printf("This is " RED "red" RESET " and this is " BLU "blue" RESET "\n");
+     * 
+     */
     #define ColorRed "\x1B[31m"
     #define ColorGreen "\x1B[32m"
     #define ColorYelow "\x1B[33m"
@@ -12,6 +18,8 @@
     #define ColorWhite   "\x1B[37m"
     #define ColorReset "\x1B[0m"
 
+    
+
     /**
      * 把接下來printf的字都變成該顏色
      * 要自己call reserTextColor調回來
@@ -21,6 +29,7 @@
      * resetTextColor();
      */
     void setTextColor(const char* colorFormat);
+    
     /**
      * 把顏色改回來
      * 跟setTextColor(ColorReset);
